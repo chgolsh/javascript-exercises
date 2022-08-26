785a036cdb3df13dae3271e50b003dbdd58e3268
Chingiz Golyshev <chgolsh@yandex.ru>
2022-08-26 21:23:57 +0700

    Add create reverse string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return 'ERROR';
+    let newStr = '';
+    for (let idx = 0; idx < times; idx++) {
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
