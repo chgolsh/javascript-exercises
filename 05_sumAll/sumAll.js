0eda9c40a94dedd2e488be4d86dbc09b3291f378
Chingiz Golyshev <chgolsh@yandex.ru>
2022-09-02 13:03:57 +0700

    Add and test solution to sumAll()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end){
+    let sum = 0;
+    if ((start < 0 || end < 0) || 
+        !(typeof(start) === "number" && typeof(end) === "number")){
+            return "ERROR";
+        }
+    if (start > end) [start, end] = [end, start];
+    for (let num = start; num <= end; num++){
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
