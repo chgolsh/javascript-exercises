41b8c1db9aceb6e571c64acf5d9c94e7a6f9d88e
Chingiz Golyshev <chgolsh@yandex.ru>
2022-09-02 13:45:40 +0700

    Add and test ftoc(), ctof()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
+  let roundedToOneDecimal = Math.round(c * 10) / 10;
+  return roundedToOneDecimal;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * (9/5)) + 32;
+  let roundedToOneDecimal = Math.round(f * 10) / 10;
+  return roundedToOneDecimal;
 };
 
 // Do not edit below this line
