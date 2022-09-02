ca4c2ba6f1781f7194cb6375c0773b1096d435b4
Chingiz Golyshev <chgolsh@yandex.ru>
2022-09-02 12:33:24 +0700

    Add and test removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(sourceArray, ...values) {
+    let array = Array.from(sourceArray);
+    for (value of values) {
+        let indexOfValue = array.indexOf(value);
+        if (indexOfValue === -1) continue;
+        array.splice(indexOfValue, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
