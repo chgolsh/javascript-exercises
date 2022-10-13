fbad0465e2b16a905a07cfe662fbf04bfbab6181
Chingiz Golyshev <chgolsh@yandex.ru>
2022-10-13 16:42:01 +0700

    Add palindrom function solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normal_str = str.replace(/[!,\.\s]/g, '').toLowerCase();
+    let reverse_str = normal_str.split('').reverse().join('');
+    return normal_str === reverse_str;
 };
 
 // Do not edit below this line
